13db89b724e27028a00cb7a08c91da83cd87fb5a
Aditya Singh <131813027+adityaSingh6271@users.noreply.github.com>
2024-01-25 11:33:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 //your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const divs = document.querySelectorAll('.image');
+
+  let draggedItem = null;
+
+  divs.forEach(div => {
+    div.addEventListener('dragstart', dragStart);
+    div.addEventListener('dragover', dragOver);
+    div.addEventListener('dragenter', dragEnter);
+    div.addEventListener('dragleave', dragLeave);
+    div.addEventListener('drop', dragDrop);
+    div.addEventListener('dragend', dragEnd);
+  });
+
+  function dragStart() {
+    draggedItem = this;
+    setTimeout(() => {
+      this.style.display = 'none';
+    }, 0);
+  }
+
+  function dragOver(e) {
+    e.preventDefault();
+  }
+
+  function dragEnter() {
+    this.classList.add('selected');
+  }
+
+  function dragLeave() {
+    this.classList.remove('selected');
+  }
+
+  function dragDrop() {
+    if (draggedItem !== this) {
+      const temp = this.innerHTML;
+      this.innerHTML = draggedItem.innerHTML;
+      draggedItem.innerHTML = temp;
+    }
+
+    this.classList.remove('selected');
+  }
+
+  function dragEnd() {
+    this.style.display = 'flex';
+    draggedItem = null;
+  }
+});
